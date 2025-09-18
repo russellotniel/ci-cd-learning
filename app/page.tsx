4dd4c49381b6325d5f1f3fce0eac455ba83f6781
Russell Otniel <russellotniel@gmail.com>
2025-09-18 15:21:35 +0700

    fix: bump version - Bump naikin ke v0.2.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <p>Hello World coba tambahin feature lagi.</p>
+      <p>Hello World coba tambahin patch untuk jadi v0.2.1.</p>
     </main>
   );
 }

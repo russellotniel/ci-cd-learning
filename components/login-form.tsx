31d2b411b9f9d2fd4774702820a48b14354238aa
Russell Otniel <58100565+russellotniel@users.noreply.github.com>
2025-09-18 20:06:47 +0700

    feat: adjusted main, protected landing pages and login form
    * feat: edited page.tsx to add paragraph
    
    - Added a sentence
    
    * feat: edited protected page
    
    - Changed the content so that it only shows the text 'Protected Page'
    
    * feat: changed Login Form Component
    
    - Adjusted the copywriting

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -53,7 +53,7 @@ export function LoginForm({
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Enter your email
           </CardDescription>
         </CardHeader>
         <CardContent>

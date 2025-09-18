e2b17e52eaf1e3eb77bad70ba0a733b58ff02b88
Russell Otniel <58100565+russellotniel@users.noreply.github.com>
2025-09-18 20:06:47 +0700

    feat: adjusted main, protected landing pages and login form
    * feat: edited page.tsx to add paragraph
    
    - Added a sentence
    
    * feat: edited protected page
    
    - Changed the content so that it only shows the text 'Protected Page'
    
    * feat: changed Login Form Component
    
    - Adjusted the copywriting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <p>Hello World coba tambahin patch untuk jadi v0.2.2.</p>
+      <p>An awesome feature made by me.</p>
     </main>
   );
 }

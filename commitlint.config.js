d382986fff0b7a5b0b2bef2e7c6b72218596167f
Russell Otniel <russellotniel@gmail.com>
2025-09-18 11:00:04 +0700

    fix: allow merge commits in commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  ignores: [
+    // Ignore merge commits
+    (message) => message.startsWith('merge:'),
+    // Ignore auto-generated release commits
+    (message) => message.startsWith('chore(release):')
+  ]
 };

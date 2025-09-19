4cd6b002e7aabe8539b0d7ea0f3a64d7ef3f50b8
Russell Otniel <58100565+russellotniel@users.noreply.github.com>
2025-09-19 09:26:25 +0700

    feat: Feat/another feature (#29)
    * feat: create another feature
    
    - Nyoba nambahin feature baru buat commit pertama
    
    * fix: pura pura benerin
    
    - Nambahin tulisan Nambahin fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <p>Pura-pura fix an awesome feature made by me.</p>
+      <p>Nambahin another feature. Nambahin fix</p>
     </main>
   );
 }

ce69a0139b46f05273851d0593e1cd19b8e49caf
Russell Otniel <russellotniel@gmail.com>
2025-09-19 13:32:13 +0700

    fix: coba fix 1.2.1
    - Coba fix untuk naikin ke 1.2.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <p>Coba bikin fix baru bray.</p>
+      <p>Coba bikin fix bray.</p>
     </main>
   );
 }
